5e88d342924dd698e69e712f003ed834f5c3c9f3
cchacon-rt <rebeca.citlali.torres@cua.uam.mx>
2026-03-24 19:20:39 -0600

    Actualizacion de rutas relativas y carpetas

---
+++ JS/functions.js
@@ -0,0 +1,21 @@
+function promotor(){
+    window.location.href="vacantes/promotor.html";
+}
+
+function callcenter(){
+    window.location.href="vacantes/callcenter.html";
+}
+
+function ayudanteGeneral(){
+    window.location.href="vacantes/ayudanteGeneral.html";
+}
+
+function seguridad(){
+    window.location.href="vacantes/seguridad.html";
+}
+
+
+
+function postularme(){
+    window.location.href="postulante/postulacion.html";
+}
